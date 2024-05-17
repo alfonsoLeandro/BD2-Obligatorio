ca0a29d0a242fad5ebefe90b3f40088954efbe6d
Leandro Alfonso <leandroalfonsoporley@gmail.com>
2024-05-17 20:22:13 -0300

    Agrega proyecto backend inicial.

---
+++ backend/src/main/java/com/github/alfonsoleandro/pencaucu/PencaUcuApplication.java
@@ -0,0 +1,13 @@
+package com.github.alfonsoleandro.pencaucu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PencaUcuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PencaUcuApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/alfonsoleandro/pencaucu/PencaUcuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.alfonsoleandro.pencaucu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PencaUcuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
