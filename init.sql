7aa33fc0947838aca8338e690292ab0f7279f696
Amanda Seara <amandadenissese@gmail.com>
2024-06-03 20:21:39 -0300

    ids

--- init.sql
+++ init.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXIST admin(
-    id_admin INT PRIMARY KEY,
+    id INT PRIMARY KEY,
     ci VARCHAR(8) NOT NULL,
     nombre VARCHAR(20) NOT NULL,
     apellido VARCHAR(20) NOT NULL,
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXIST admin(
 );
 
 CREATE TABLE IF NOT EXIST alumno(
-    id_alumno INT PRIMARY KEY,
+    id INT PRIMARY KEY,
     ci VARCHAR(8) NOT NULL,
     nombre VARCHAR(20) NOT NULL,
     apellido VARCHAR(20) NOT NULL,
@@ -20,51 +20,51 @@ CREATE TABLE IF NOT EXIST alumno(
 );
 
 CREATE TABLE IF NOT EXIST carrera(
-    id_carrera INT PRIMARY KEY,
+    id INT PRIMARY KEY,
     nombre_carrera VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE IF NOT EXIST equipo(
-    id_equipo INT PRIMARY KEY,
+    id INT PRIMARY KEY,
     nombre_equipo VARCHAR(50) NOT NULL,
-    ranking VARCHAR(50)
+    ranking INT
 );
 
 CREATE TABLE IF NOT EXIST partido(
-    id_partido INT PRIMARY KEY,
+    id INT PRIMARY KEY,
     fecha DATE NOT NULL
 );
 
 CREATE TABLE IF NOT EXIST etapa(
-    id_etapa INT PRIMARY KEY,
+    id INT PRIMARY KEY,
     nombre_etapa VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE IF NOT EXIST juega(
-    id_juega INT PRIMARY KEY,
+    id INT PRIMARY KEY,
     goles INT NOT NULL
 );
 
 CREATE TABLE IF NOT EXIST elige(
-    id_elige INT PRIMARY KEY
+    id INT PRIMARY KEY
 );
 
 CREATE TABLE IF NOT EXIST predice(
-    id_predice INT PRIMARY KEY,
+    id INT PRIMARY KEY,
     goles INT NOT NULL
 );
 
-ALTER TABLE alumno ADD FOREIGN KEY id_carrera REFERENCES carrera(id_carrera);
+ALTER TABLE alumno ADD FOREIGN KEY id_carrera REFERENCES carrera(id);
 
-ALTER TABLE partido ADD FOREIGN KEY id_etapa REFERENCES etapa(id_etapa);
+ALTER TABLE partido ADD FOREIGN KEY id_etapa REFERENCES etapa(id);
 
-ALTER TABLE juega ADD FOREIGN KEY id_equipo REFERENCES equipo(id_equipo);
-ALTER TABLE juega ADD FOREIGN KEY id_partido REFERENCES partido(id_partido);
+ALTER TABLE juega ADD FOREIGN KEY id_equipo REFERENCES equipo(id);
+ALTER TABLE juega ADD FOREIGN KEY id_partido REFERENCES partido(id);
 
-ALTER TABLE elige ADD FOREIGN KEY id_alumno REFERENCES alumno(id_alumno);
-ALTER TABLE elige ADD FOREIGN KEY campeon REFERENCES equipo(id_equipo);
-ALTER TABLE elige ADD FOREIGN KEY subcampeon REFERENCES equipo(id_equipo);
+ALTER TABLE elige ADD FOREIGN KEY id_alumno REFERENCES alumno(id);
+ALTER TABLE elige ADD FOREIGN KEY campeon REFERENCES equipo(id);
+ALTER TABLE elige ADD FOREIGN KEY subcampeon REFERENCES equipo(id);
 
-ALTER TABLE predice ADD FOREIGN KEY idPartido REFERENCES patido(id_partido);
-ALTER TABLE predice ADD FOREIGN KEY idEquipo REFERENCES equipo(id_equipo);
-ALTER TABLE predice ADD FOREIGN KEY idAlumno REFERENCES alumno(id_alumno);
+ALTER TABLE predice ADD FOREIGN KEY idPartido REFERENCES patido(id);
+ALTER TABLE predice ADD FOREIGN KEY idEquipo REFERENCES equipo(id);
+ALTER TABLE predice ADD FOREIGN KEY idAlumno REFERENCES alumno(id);
