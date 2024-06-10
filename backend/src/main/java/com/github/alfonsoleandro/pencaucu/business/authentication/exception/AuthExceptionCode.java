427a4c31d979d62e46174dc34ea0abb674c8efb2
Leandro Alfonso <leandroalfonsoporley@gmail.com>
2024-06-09 22:50:16 -0300

    Agrega AuthExceptionCode.

---
+++ backend/src/main/java/com/github/alfonsoleandro/pencaucu/business/authentication/exception/AuthExceptionCode.java
@@ -0,0 +1,12 @@
+package com.github.alfonsoleandro.pencaucu.business.authentication.exception;
+
+import com.github.alfonsoleandro.pencaucu.business.exception.ExceptionCode;
+
+/**
+ * @author alfonsoLeandro
+ * @version 0.0.1
+ */
+public enum AuthExceptionCode implements ExceptionCode {
+
+    USUARIO_YA_EXISTE;
+}
