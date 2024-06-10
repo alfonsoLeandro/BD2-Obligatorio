76f841c7304b56b6f17fefeafab14063eb7c957f
Leandro Alfonso <leandroalfonsoporley@gmail.com>
2024-06-09 22:50:07 -0300

    Agrega CarreraRepository.

---
+++ backend/src/main/java/com/github/alfonsoleandro/pencaucu/persistence/repository/CarreraRepository.java
@@ -0,0 +1,13 @@
+package com.github.alfonsoleandro.pencaucu.persistence.repository;
+
+import com.github.alfonsoleandro.pencaucu.persistence.entity.Carrera;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface CarreraRepository extends JpaRepository<Carrera, Integer> {
+
+  @Query(value = "SELECT c.* FROM carrera c WHERE c.id = :id", nativeQuery = true)
+  Optional<Carrera> findById(int id);
+}
