3df5f017359d4710a6d9cf99b79d13cd57a23da1
Leandro Alfonso <leandroalfonsoporley@gmail.com>
2024-06-10 00:55:23 -0300

    Agrega errores de validacion y genéricos a AppExceptionHandler.

--- backend/src/main/java/com/github/alfonsoleandro/pencaucu/core/exception/AppExceptionHandler.java
+++ backend/src/main/java/com/github/alfonsoleandro/pencaucu/core/exception/AppExceptionHandler.java
@@ -2,7 +2,9 @@ package com.github.alfonsoleandro.pencaucu.core.exception;
 
 import com.github.alfonsoleandro.pencaucu.rest.exception.ApiException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -22,9 +24,29 @@ public class AppExceptionHandler {
     @ResponseBody
     @ExceptionHandler(ApiException.class)
     public ResponseEntity<ErrorApiResponse> apiExceptionHandler(ApiException ex, WebRequest request) {
+        log.error("Api exception", ex);
         return ResponseEntity
                 .status(ex.getHttpStatus())
                 .body(buildErrorApiResponse(ex.getMessage(), request));
+    }
+
+    @ResponseBody
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorApiResponse> validationExceptionHandler(MethodArgumentNotValidException ex, WebRequest request) {
+        log.error("Validation exception", ex);
+        String firstErrorMessage = ex.getBindingResult().getAllErrors().getFirst().getDefaultMessage();
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(buildErrorApiResponse(firstErrorMessage, request));
+    }
+
+    @ResponseBody
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorApiResponse> genericExceptionHandler(Exception ex, WebRequest request) {
+        log.error("Internal server error", ex);
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(buildErrorApiResponse("ERROR_INESPERADO", request));
     }
 
     private ErrorApiResponse buildErrorApiResponse(String message, WebRequest request) {
