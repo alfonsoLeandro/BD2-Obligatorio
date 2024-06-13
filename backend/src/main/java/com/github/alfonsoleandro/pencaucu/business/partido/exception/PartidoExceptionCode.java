b3165ac4f5567a3fdb2773b4af962d1af0a9b7fc
Leandro Alfonso <leandroalfonsoporley@gmail.com>
2024-06-13 20:44:16 -0300

    Agrega PartidoExceptionCode.

---
+++ backend/src/main/java/com/github/alfonsoleandro/pencaucu/business/partido/exception/PartidoExceptionCode.java
@@ -0,0 +1,13 @@
+package com.github.alfonsoleandro.pencaucu.business.partido.exception;
+
+import com.github.alfonsoleandro.pencaucu.business.exception.ExceptionCode;
+
+/**
+ * @author alfonsoLeandro
+ * @version 0.0.1
+ */
+public enum PartidoExceptionCode implements ExceptionCode {
+
+    PARTIDO_NO_ENCONTRADO
+
+}
