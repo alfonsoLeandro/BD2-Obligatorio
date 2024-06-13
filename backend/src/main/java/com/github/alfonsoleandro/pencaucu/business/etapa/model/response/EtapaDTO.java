5357c5e232afef1e6e1ee7cab4f72d135a341c3a
Leandro Alfonso <leandroalfonsoporley@gmail.com>
2024-06-13 17:06:12 -0300

    Agrega EtapaDTO.

---
+++ backend/src/main/java/com/github/alfonsoleandro/pencaucu/business/etapa/model/response/EtapaDTO.java
@@ -0,0 +1,18 @@
+package com.github.alfonsoleandro.pencaucu.business.etapa.model.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author alfonsoLeandro
+ * @version 0.0.1
+ */
+@Getter
+@Setter
+public class EtapaDTO {
+
+    private int id;
+
+    private String nombre;
+
+}
