889b6594a7342c9c2759799a6103e93dd234cf9a
Leandro Alfonso <leandroalfonsoporley@gmail.com>
2024-06-14 00:56:47 -0300

    Agrega viewToDetailsDTO.

--- backend/src/main/java/com/github/alfonsoleandro/pencaucu/business/partido/mapper/PartidoMapper.java
+++ backend/src/main/java/com/github/alfonsoleandro/pencaucu/business/partido/mapper/PartidoMapper.java
@@ -1,10 +1,10 @@
 package com.github.alfonsoleandro.pencaucu.business.partido.mapper;
 
 import com.github.alfonsoleandro.pencaucu.business.partido.model.response.PartidoDTO;
+import com.github.alfonsoleandro.pencaucu.business.partido.model.response.PartidoDetailsDTO;
 import com.github.alfonsoleandro.pencaucu.persistence.view.PartidoSearchView;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-
 
 /**
  * @author alfonsoLeandro
@@ -24,7 +24,20 @@ public interface PartidoMapper {
     @Mapping(source = "nombreEquipo2", target = "equipo2.nombre")
     @Mapping(source = "golesEquipo2", target = "equipo2.goles")
     @Mapping(source = "prediccionEquipo2", target = "equipo2.prediccion")
-    PartidoDTO toDTO(PartidoSearchView view);
     PartidoDTO viewToDTO(PartidoSearchView view);
+
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "idEtapa", target = "etapa.id")
+    @Mapping(source = "nombreEtapa", target = "etapa.nombre")
+    @Mapping(source = "idEquipo1", target = "equipo1.id")
+    @Mapping(source = "nombreEquipo1", target = "equipo1.nombre")
+    @Mapping(source = "golesEquipo1", target = "equipo1.goles")
+    @Mapping(source = "prediccionEquipo1", target = "equipo1.prediccion")
+    @Mapping(source = "idEquipo2", target = "equipo2.id")
+    @Mapping(source = "nombreEquipo2", target = "equipo2.nombre")
+    @Mapping(source = "golesEquipo2", target = "equipo2.goles")
+    @Mapping(source = "prediccionEquipo2", target = "equipo2.prediccion")
+    @Mapping(target = "predicciones", ignore = true)
+    PartidoDetailsDTO viewToDetailsDTO(PartidoSearchView view);
 
 }
