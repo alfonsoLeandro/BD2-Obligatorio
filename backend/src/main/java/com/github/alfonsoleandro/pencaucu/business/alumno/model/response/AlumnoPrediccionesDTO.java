bc0cffd1c336cc81bc03bb160da9b69cb46418fc
Leandro Alfonso <leandroalfonsoporley@gmail.com>
2024-06-14 00:51:50 -0300

    Agrega AlumnoPrediccionesDTO.

---
+++ backend/src/main/java/com/github/alfonsoleandro/pencaucu/business/alumno/model/response/AlumnoPrediccionesDTO.java
@@ -0,0 +1,25 @@
+package com.github.alfonsoleandro.pencaucu.business.alumno.model.response;
+
+import com.github.alfonsoleandro.pencaucu.business.carrera.model.response.CarreraDTO;
+import com.github.alfonsoleandro.pencaucu.business.equipo.model.response.EquipoPrediccionDTO;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author alfonsoLeandro
+ * @version 0.0.1
+ */
+@Getter
+@Setter
+public class AlumnoPrediccionesDTO {
+
+    private Integer idAlumno;
+
+    private String nombreAlumno;
+
+    private CarreraDTO carrera;
+
+    private EquipoPrediccionDTO equipo1;
+
+    private EquipoPrediccionDTO equipo2;
+}
