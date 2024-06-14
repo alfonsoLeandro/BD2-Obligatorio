5931a2da750940258551a4b09fc3bbe63680c2a7
Leandro Alfonso <leandroalfonsoporley@gmail.com>
2024-06-13 23:10:24 -0300

    Agrega CarreraDTO.

---
+++ backend/src/main/java/com/github/alfonsoleandro/pencaucu/business/carrera/model/response/CarreraDTO.java
@@ -0,0 +1,17 @@
+package com.github.alfonsoleandro.pencaucu.business.carrera.model.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author alfonsoLeandro
+ * @version 0.0.1
+ */
+@Getter
+@Setter
+public class CarreraDTO {
+
+    private Integer id;
+
+    private String nombre;
+}
