c12492d5038170b32fddadda925286daadaadaca
Leandro Alfonso <leandroalfonsoporley@gmail.com>
2024-06-14 00:51:24 -0300

    Agrega AlumnosPrediccionesView.

---
+++ backend/src/main/java/com/github/alfonsoleandro/pencaucu/persistence/view/AlumnosPrediccionesView.java
@@ -0,0 +1,29 @@
+package com.github.alfonsoleandro.pencaucu.persistence.view;
+
+/**
+ * @author alfonsoLeandro
+ * @version 0.0.1
+ */
+public interface AlumnosPrediccionesView {
+
+	Integer getIdAlumno();
+
+	String getNombreAlumno();
+
+	Integer getIdCarrera();
+
+	String getNombreCarrera();
+
+	Integer getIdEquipo1();
+
+	String getNombreEquipo1();
+
+	Integer getPrediccionEquipo1();
+
+	Integer getIdEquipo2();
+
+	String getNombreEquipo2();
+
+	Integer getPrediccionEquipo2();
+
+}
