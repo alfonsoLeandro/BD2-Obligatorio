bf427241ffbb1fc0b185e62ea2e401b9cabaaabc
Leandro Alfonso <leandroalfonsoporley@gmail.com>
2024-06-14 20:57:11 -0300

    Agrega EquipoResultDTO.

---
+++ backend/src/main/java/com/github/alfonsoleandro/pencaucu/business/equipo/model/request/EquipoResultDTO.java
@@ -0,0 +1,23 @@
+package com.github.alfonsoleandro.pencaucu.business.equipo.model.request;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author alfonsoLeandro
+ * @version 0.0.1
+ */
+@Getter
+@Setter
+public class EquipoResultDTO {
+
+	@NotNull(message = "ID_EQUIPO_REQUERIDO")
+	private Integer id;
+
+	@NotNull(message = "ID_EQUIPO_REQUERIDO")
+	@Min(value = 0, message = "GOLES_DEBE_SER_POSITIVO")
+	private Integer goles;
+
+}
