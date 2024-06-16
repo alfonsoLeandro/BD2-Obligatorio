0bed374a4051eae85743635c04fbee69d23518a0
Leandro Alfonso <leandroalfonsoporley@gmail.com>
2024-06-16 16:11:22 -0300

    Agrega query para obtener alumno según ID de usuario.

--- backend/src/main/java/com/github/alfonsoleandro/pencaucu/persistence/repository/AlumnoRepository.java
+++ backend/src/main/java/com/github/alfonsoleandro/pencaucu/persistence/repository/AlumnoRepository.java
@@ -2,7 +2,21 @@ package com.github.alfonsoleandro.pencaucu.persistence.repository;
 
 import com.github.alfonsoleandro.pencaucu.persistence.entity.Alumno;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface AlumnoRepository extends JpaRepository<Alumno, Integer> {
+
+    @Query(value = """
+            SELECT a.*
+            FROM alumnos a
+                     JOIN usuarios u ON a.id_usuario = u.id
+            WHERE a.id_usuario = :idUsuario
+              AND u.role = 0
+            """, nativeQuery = true)
+    Optional<Alumno> findByUsuarioId(int idUsuario);
 
 }
