dae94144cc75449fec42380e5dfb062743ca7c35
Leandro Alfonso <leandroalfonsoporley@gmail.com>
2024-06-18 20:31:40 -0300

    Agrega CampeonSubcampeonView.

---
+++ backend/src/main/java/com/github/alfonsoleandro/pencaucu/persistence/view/CampeonSubcampeonView.java
@@ -0,0 +1,13 @@
+package com.github.alfonsoleandro.pencaucu.persistence.view;
+
+/**
+ * @author alfonsoLeandro
+ * @version 0.0.1
+ */
+public interface CampeonSubcampeonView {
+
+	Integer getIdCampeon();
+
+	Integer getIdSubcampeon();
+
+}
