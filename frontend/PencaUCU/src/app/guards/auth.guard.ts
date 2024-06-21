51059c5e77b59b2f8ea19b03414df36468d88c1f
Leandro Alfonso <leandroalfonsoporley@gmail.com>
2024-06-21 18:29:00 -0300

    Implementa auth guard temporal.

--- frontend/PencaUCU/src/app/guards/auth.guard.ts
+++ frontend/PencaUCU/src/app/guards/auth.guard.ts
@@ -1,28 +1,24 @@
-import {CanActivateFn, Router} from '@angular/router';
-import {inject} from "@angular/core";
-import { AuthService } from '../services/auth.service';
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
 import { firstValueFrom } from 'rxjs';
+import { PartidoService } from '../services/partido.service';
 
 export const authGuard: CanActivateFn = async (route, state) => {
-  const token = localStorage.getItem('token');
-  const router = inject(Router);
-  const authService = inject(AuthService);
+    const token = localStorage.getItem('token');
+    const router = inject(Router);
+    const partidoService = inject(PartidoService);
 
-  if(token) {
-    try {
-      const isValid = await firstValueFrom(authService.validateToken(token));
-      if (isValid) {
-        return true;
-      } else {
+    if (token) {
+        try {
+            // Check if a secure endpoint is accessible using the current token.
+            await firstValueFrom(partidoService.getPartidos());
+            return true;
+        } catch (error) {
+            await router.navigate(['/login']);
+            return false;
+        }
+    } else {
         await router.navigate(['/login']);
         return false;
-      }
-    } catch (error) {
-      await router.navigate(['/login']);
-      return false;
     }
-  } else {
-    await router.navigate(['/login']);
-    return false;
-  }
 };
