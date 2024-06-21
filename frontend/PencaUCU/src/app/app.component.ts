69f9c9db5875ac0981270e66bc5333e65f11d55a
Leandro Alfonso <leandroalfonsoporley@gmail.com>
2024-06-21 20:50:40 -0300

    Agrega angular localize para la fecha en español.

--- frontend/PencaUCU/src/app/app.component.ts
+++ frontend/PencaUCU/src/app/app.component.ts
@@ -1,18 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, LOCALE_ID } from '@angular/core';
+import '@angular/common/locales/global/es-UY';
 import { RouterOutlet } from '@angular/router';
-import { HttpClientModule} from "@angular/common/http";
-import {CommonModule} from "@angular/common";
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [
-    RouterOutlet,
-    CommonModule,
-    HttpClientModule,
-  ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    imports: [
+        RouterOutlet,
+        CommonModule,
+        HttpClientModule,
+    ],
+    providers: [{ provide: LOCALE_ID, useValue: 'es-UY' }],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
 }
