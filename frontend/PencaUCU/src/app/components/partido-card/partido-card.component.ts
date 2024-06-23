3606663b66e04d99c0ad66c79f52dfed919cf005
Leandro Alfonso <leandroalfonsoporley@gmail.com>
2024-06-23 05:02:49 -0300

    Agrega colores a las predicciones segun si fueron acertadas.

--- frontend/PencaUCU/src/app/components/partido-card/partido-card.component.ts
+++ frontend/PencaUCU/src/app/components/partido-card/partido-card.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { PartidoApiDto } from '../../models/partido-api-dto';
 import { FlagService } from '../../services/flag.service';
-import { DatePipe } from '@angular/common';
+import { DatePipe, NgClass } from '@angular/common';
 import { MatButton } from '@angular/material/button';
 
 @Component({
@@ -9,7 +9,8 @@ import { MatButton } from '@angular/material/button';
     standalone: true,
     imports: [
         DatePipe,
-        MatButton
+        MatButton,
+        NgClass
     ],
     templateUrl: './partido-card.component.html',
     styleUrl: './partido-card.component.scss'
@@ -27,6 +28,47 @@ export class PartidoCardComponent implements OnInit {
         this.equipo1FlagPath = this.flagService.getFlagPath(this.partido.equipo1.id);
         this.equipo2FlagPath = this.flagService.getFlagPath(this.partido.equipo2.id);
 
+    }
+
+    resultadoErroneo() {
+        const pred1 = this.partido.equipo1.prediccion;
+        const pred2 = this.partido.equipo2.prediccion;
+        const goles1 = this.partido.equipo1.goles;
+        const goles2 = this.partido.equipo2.goles;
+
+        if (pred1 == null || pred2 == null || goles1 == null || goles2 == null) {
+            return false;
+        }
+
+        return (pred1 > pred2 !== goles1 > goles2) || (pred1 == pred2 && goles1 != goles2) || (pred1 != pred2 && goles1 == goles2);
+
+    }
+
+    resultadoCercano() {
+        const pred1 = this.partido.equipo1.prediccion;
+        const pred2 = this.partido.equipo2.prediccion;
+        const goles1 = this.partido.equipo1.goles;
+        const goles2 = this.partido.equipo2.goles;
+
+        if (pred1 == null || pred2 == null || goles1 == null || goles2 == null) {
+            return false;
+        }
+
+        return (pred1 > goles1) == (pred2 > goles2);
+
+    }
+
+    resultadoExacto() {
+        const pred1 = this.partido.equipo1.prediccion;
+        const pred2 = this.partido.equipo2.prediccion;
+        const goles1 = this.partido.equipo1.goles;
+        const goles2 = this.partido.equipo2.goles;
+
+        if (pred1 == null || pred2 == null || goles1 == null || goles2 == null) {
+            return false;
+        }
+
+        return pred1 == goles1 && pred2 == goles2;
     }
 
 }
